591d47a0878d9988a3539cddeca669e4c4cd46a5
ABSerran <wiskosin@gmail.com>
2026-01-14 20:10:37 +0100

    Firebase configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -29,9 +30,18 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures{
+        viewBinding = true
+
+    }
 }
 
 dependencies {
+//Firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
 
     implementation(libs.appcompat)
     implementation(libs.material)
